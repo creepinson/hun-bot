91222bb98ffd8a79eea75f2362a19bd0ef1819aa
Theo Paris <theo@throw-out-error.dev>
2020-08-07 19:39:35 -0700

    cleaned up typescript files

--- index.ts
+++ index.ts
@@ -3,21 +3,23 @@ import Discord from "discord.js";
 import * as utils from "./utilities";
 
 env.config();
-const token:string = <string>process.env.TOKEN;
-const bot:Discord.Client = new Discord.Client();
+const token: string = <string>process.env.TOKEN;
+const bot: Discord.Client = new Discord.Client();
 
-bot.on("ready", ()=>console.log("Logged in!"));
-bot.on("message", message => {
+bot.on("ready", () => console.log("Logged in!"));
+bot.on("message", (message) => {
     if (message.author.bot) return;
-    const content:string = message.content;
-    let words = content.replace(/\s+/g,' ').split(' ');
+    const content: string = message.content;
+    let words = content.replace(/\s+/g, " ").split(" ");
     switch (words[0]) {
         case "!sparklize":
             utils.deleteMessage(message);
-            utils.sendMessage(message.channel,content.replace(/!sparklize\s+/,""));
+            utils.sendMessage(
+                message.channel,
+                content.replace(/!sparklize\s+/, ""),
+            );
             break;
     }
-    
 });
 
 bot.login(token);
--- utilities.ts
+++ utilities.ts
@@ -1,24 +1,26 @@
 import Discord from "discord.js";
 
-const sparklesExp:RegExp = new RegExp([
-    /hun/,
-    /(?:essential\s+)?oils/,
-    /essential/
-].map(s=>`(?:${String(s).slice(1,-1)})`).join('|'), "gi")
+const sparklesExp: RegExp = new RegExp(
+    [/hun/, /(?:essential\s+)?oils/, /essential/]
+        .map((s) => `(?:${String(s).slice(1, -1)})`)
+        .join("|"),
+    "gi",
+);
 
-export const sparklize = (s:string):string =>
-    s.replace(sparklesExp,":sparkles:$&:sparkles:");
+export const sparklize = (s: string): string =>
+    s.replace(sparklesExp, ":sparkles:$&:sparkles:");
 
-export const reply = (message:Discord.Message,reply:string):Promise<Discord.Message> =>
-    message.reply(sparklize(reply));
+export const reply = (
+    message: Discord.Message,
+    reply: string,
+): Promise<Discord.Message> => message.reply(sparklize(reply));
 
 export const sendMessage = (
-        channel:Discord.TextChannel | Discord.DMChannel | Discord.NewsChannel,
-        message:string
-    ):Promise<Discord.Message> =>
-        channel.send(sparklize(message));
+    channel: Discord.TextChannel | Discord.DMChannel | Discord.NewsChannel,
+    message: string,
+): Promise<Discord.Message> => channel.send(sparklize(message));
 
 export const deleteMessage = (
-        message:Discord.Message
-    ):Promise<Discord.Message> | void =>
-        message.deletable?message.delete():undefined
+    message: Discord.Message,
+): Promise<Discord.Message> | void =>
+    message.deletable ? message.delete() : undefined;
